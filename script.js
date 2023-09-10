f49af2cd8a01291b959f7d9156eb4d0e7a50a5f8
lkua0002 <lkua0002@student.monash.edu>
2023-09-11 01:48:40 +0800

    display created tasks details

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const closeFloatingWindow = document.getElementById("closeFloatingWindow");
   const saveTaskButton = document.getElementById("saveTaskButton");
   const taskList = document.getElementById("taskList");
+  const taskDetailsWindow = document.getElementById("taskDetailsWindow");
+  const closeTaskDetails = document.getElementById("closeTaskDetails");
+
 
   addTaskButton.addEventListener("click", function () {
     floatingWindow.style.display = "block"; // Show the floating window
@@ -24,24 +27,37 @@ document.addEventListener("DOMContentLoaded", function () {
     const taskDescription = document.getElementById("taskDescription").value;
     const taskStatus = document.getElementById("taskStatus").value;
 
+     // Create a new task object
+     const task = {
+      name: taskName,
+      category: category,
+      tag: tag,
+      priority: priority,
+      assignee: assignee,
+      storyPoint: storyPoint,
+      taskDescription: taskDescription,
+      taskStatus: taskStatus,
+    };
+
     // Create a new task container
     const taskContainer = document.createElement("div");
     taskContainer.classList.add("task-container");
-
+    
     // Create a task box with dynamic content
     const taskBox = document.createElement("div");
     taskBox.classList.add("task-box");
     taskBox.innerHTML = `
-      <h2>${taskName}</h2>
-      <p><strong>Task Name:</strong> ${taskName}</p>
-      <p><strong>Category:</strong> ${category}</p>
+      <p><strong>Name:</strong> ${taskName}</p>
       <p><strong>Tag:</strong> ${tag}</p>
-      <p><strong>Priority:</strong> ${priority}</p>
-      <p><strong>Assignee:</strong> ${assignee}</p>
       <p><strong>Story Point:</strong> ${storyPoint}</p>
-      <p><strong>Description:</strong> ${taskDescription}</p>
-      <p><strong>Status:</strong> ${taskStatus}</p>
+      <p><strong>Priority:</strong> ${priority}</p>
     `;
+
+    // Add a click event listener to each task container
+    taskContainer.addEventListener("click", function () {
+      // Display task details in the task details window when a task is clicked
+      displayTaskDetails(task);
+    });
 
     // Append the task box to the task container
     taskContainer.appendChild(taskBox);
@@ -49,6 +65,24 @@ document.addEventListener("DOMContentLoaded", function () {
     // Append the task container to the task list container
     taskList.appendChild(taskContainer);
     
+    // Function to display task details in the task details window
+    function displayTaskDetails(task) {
+      const taskDetailsContent = document.getElementById("taskDetailsContent");
+      taskDetailsContent.innerHTML = `
+        <p><strong>Name:</strong> ${task.name}</p>
+        <p><strong>Category:</strong> ${category}</p>
+        <p><strong>Tag:</strong> ${task.tag}</p>
+        <p><strong>Priority:</strong> ${task.priority}</p>
+        <p><strong>Assignee:</strong> ${assignee}</p>
+        <p><strong>Story Point:</strong> ${task.storyPoint}</p>
+        <p><strong>Description:</strong> ${taskDescription}</p>
+        <p><strong>Status:</strong> ${taskStatus}</p>
+      `;
+
+      // Show the task details window
+      taskDetailsWindow.style.display = "block";
+    }
+
     // Hide the floating window
     floatingWindow.style.display = "none";
 
@@ -61,6 +95,11 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("storyPoint").value = "";
     document.getElementById("taskDescription").value = "";
     document.getElementById("taskStatus").value = "todo";
+  });
+
+  // Close the task details window when the "Close" button is clicked
+  closeTaskDetails.addEventListener("click", function () {
+    taskDetailsWindow.style.display = "none";
   });
 
 
