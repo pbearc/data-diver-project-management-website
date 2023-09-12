82c40640228b3211fc626346992b10a94c64cd3a
GwinLim <127588947+GwinLim@users.noreply.github.com>
2023-09-13 00:34:03 +0800

    Fixed Edit Button

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
 import { getFirestore, collection, addDoc, doc, updateDoc, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { deleteDoc } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBbyuRShsNdaTzIcuKKzlvTDl8bCDr8pJY",
   authDomain: "fit2101-project-database.firebaseapp.com",
@@ -23,23 +20,87 @@ const app = initializeApp(firebaseConfig, "Data Diver");
 const db = getFirestore(app)
 
 document.addEventListener("DOMContentLoaded", function () {
+  let editingTaskId = null;
   
-  function displayTask(taskData) {
+  function displayTask(taskData, taskId) {
     const taskList = document.getElementById("taskList");
-
+  
     // Create HTML elements to display the task data
     const taskItem = document.createElement("div");
+    taskItem.style.backgroundColor = "white"; // Set the background color to white
+    taskItem.style.padding = "10px"; // Add some padding
+    taskItem.style.margin = "10px"; // Add margin to all sides
+    taskItem.style.borderRadius = "5px"; // Add rounded corners
+    taskItem.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.1)"; // Add a box shadow for a floating effect
+    taskItem.style.position = "relative"; // Set position to relative
+  
+    // Create the delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "X";
+    deleteButton.style.position = "absolute";
+    deleteButton.style.top = "10px";
+    deleteButton.style.right = "10px";
+  
+    // Add an event listener to the delete button
+    deleteButton.addEventListener("click", function () {
+      const confirmDelete = confirm("Do you want to delete this task?");
+      if (confirmDelete) {
+        deleteTask(taskId);
+      }
+    });
+
+    // Create the edit button
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "Edit";
+    editButton.style.position = "absolute";
+    editButton.style.bottom = "10px";
+    editButton.style.right = "10px";
+
+    // Add an event listener to the edit button
+    editButton.addEventListener("click", function () {
+      // Populate the form with the current task data
+      document.getElementById("taskName").value = taskData.taskName;
+      document.getElementById("category").value = taskData.category;
+      document.getElementById("tag").value = taskData.tag;
+      document.getElementById("priority").value = taskData.priority;
+      document.getElementById("assignee").value = taskData.assignee;
+      document.getElementById("storyPoint").value = taskData.storyPoint;
+      document.getElementById("taskDescription").value = taskData.taskDescription;
+      document.getElementById("taskStatus").value = taskData.taskStatus;
+
+      // Store the taskId being edited
+      editingTaskId = taskId;
+
+      // Show the floating window
+      floatingWindow.style.display = "block";
+    });
+
+  
     taskItem.innerHTML = `
-      <h3>${taskData.taskName}</h3>
+      <h3>${taskData.taskName}</h4>
       <p>Category: ${taskData.category}</p>
       <p>Story Point: ${taskData.storyPoint}</p>
       <p>Assignee: ${taskData.assignee}</p>
       <p>Description: ${taskData.taskDescription}</p>
       <p>Status: ${taskData.taskStatus}</p>
     `;
-
-    // Append the task item to the task list
+  
+    // Append the delete and edit buttons and the task item to the task list
+    taskItem.appendChild(deleteButton);
+    taskItem.appendChild(editButton);
     taskList.appendChild(taskItem);
+  }
+
+  async function deleteTask(taskId) {
+    const taskRef = doc(db, "tasks", taskId);
+  
+    // Delete the document
+    try {
+      await deleteDoc(taskRef);
+      console.log("Document deleted successfully!");
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+    }
   }
 
   // Update Task Function
@@ -58,19 +119,20 @@ document.addEventListener("DOMContentLoaded", function () {
   // Function to retrieve and display data in real-time
   function displayTasksRealtime() {
     const tasksCollection = collection(db, "tasks");
-
+  
     // Create a real-time listener for changes in the collection
     onSnapshot(tasksCollection, (querySnapshot) => {
       const taskList = document.getElementById("taskList");
-
+  
       // Clear the existing task list
       taskList.innerHTML = "";
-
+  
       querySnapshot.forEach((doc) => {
         const taskData = doc.data();
-
+  
         // Display each task immediately
-        displayTask(taskData);
+        // Pass the document ID as the second argument
+        displayTask(taskData, doc.id);
       });
     });
   }
@@ -131,17 +193,26 @@ document.addEventListener("DOMContentLoaded", function () {
       taskStatus,
     };
 
+    if (editingTaskId) {
+      updateTask(editingTaskId, taskData);
+
+      // Hide the floating window after updating the task
+      floatingWindow.style.display = "none";
+
+      // Reset the editingTaskId variable
+      editingTaskId = null;
+    }
+    else {
     // Add the data to Firestore
-    const tasksCollection = collection(db, "tasks"); // Replace "tasks" with your Firestore collection name
-    addDoc(tasksCollection, taskData)
-      .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-        // Optionally, you can clear the form fields or close the floating window here
-      })
-      .catch((error) => {
-        console.error("Error adding document: ", error);
-      });
-    floatingWindow.style.display = "none"; // Hide the floating window
+      const tasksCollection = collection(db, "tasks"); 
+      addDoc(tasksCollection, taskData)
+        .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+          floatingWindow.style.display = "none"; 
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+    }
   });
 });
-
